e5f15e60084a6a67e8201b7ab84c2263ffc0cede
Thamer <tmr.sa1@gmail.com>
2026-09-07 06:00:46 -0700

    Publish Unwani PWA at /unwani/ for public review

---
+++ unwani/sw.js
@@ -0,0 +1,18 @@
+const CACHE = "unwani-pages-v1";
+const ASSETS = ["./", "./index.html", "./app.js", "./unwani-engine.js", "./styles.css", "./manifest.json", "./icon.svg"];
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((hit) => hit || fetch(event.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(event.request, copy)).catch(() => undefined);
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
