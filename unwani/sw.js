0cecbfc1293cad167d86849fd4b72ae30f152e62
Thamer <tmr.sa1@gmail.com>
2026-09-07 12:18:13 -0700

    unwani 0.1.6-beta: version lock, SW cache v016, core/ops/legal pages

--- unwani/sw.js
+++ unwani/sw.js
@@ -1,10 +1,25 @@
-const CACHE = "unwani-v013";
-const ASSETS = ["./","./index.html","./app.js","./app-standalone.js","./unwani-engine.js","./styles.css","./manifest.json","./icon.svg","./apple-touch-icon.png"];
+const CACHE = "unwani-v016";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./app-standalone.js",
+  "./unwani-engine.js",
+  "./styles.css",
+  "./manifest.json",
+  "./icon.svg",
+  "./audit.html",
+  "./core.html",
+  "./ops.html",
+  "./legal.html"
+];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
 self.addEventListener("activate", (event) => {
-  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())
+  );
 });
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
